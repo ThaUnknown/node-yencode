711e29f282027f6b776285f33a05a1029226f098
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2026-04-24 12:56:09 +0200

    feat: try better prebuild action

--- y.cjs
+++ y.cjs
@@ -1,7 +1,9 @@
 module.exports = (() => {
   try {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    return require('node-gyp-build')(__dirname)
+    require.addon = require('require-addon')
+
+    return require.addon('.', __filename)
   } catch (err) {
     console.warn('yencode not supported in this environment', err)
     return {}
